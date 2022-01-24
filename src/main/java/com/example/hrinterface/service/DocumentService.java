934d58091a3dd24ccf047cbe91333aa7b46283f3
Gavin Zhou <gavinzhou98@gmail.com>
2022-01-24 10:28:06 -0500

    added file upload features

--- src/main/java/com/example/hrinterface/service/DocumentService.java
+++ src/main/java/com/example/hrinterface/service/DocumentService.java
@@ -1,6 +1,8 @@
 package com.example.hrinterface.service;
 
+import com.example.hrinterface.dao.DigitalDocumentDAO;
 import com.example.hrinterface.dao.PersonalDocumentDAO;
+import com.example.hrinterface.entity.DigitalDocument;
 import com.example.hrinterface.entity.PersonalDocument;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,8 @@ import java.util.List;
 public class DocumentService {
     @Autowired
     PersonalDocumentDAO personalDocumentDAO;
+    @Autowired
+    DigitalDocumentDAO digitalDocumentDAO;
 
     public PersonalDocument findDocumentByID(int ID){
         try{
@@ -24,5 +28,29 @@ public class DocumentService {
             return personalDocumentDAO.getDocsByEmployeeId(id);
         }catch (Exception e){System.out.println(e);}
         return null;
+    }
+
+    public DigitalDocument getDigitalDocumentById(Integer digitalDocId){
+        try {
+            return digitalDocumentDAO.getDigitalDocById(digitalDocId);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void addNewDigitalDoc(DigitalDocument digitalDocument){
+        try {
+            digitalDocumentDAO.addNewDocument(digitalDocument);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public DigitalDocument updateDigitalDoc(DigitalDocument digitalDocument){
+        if(digitalDocument == null){
+            return null;
+        }
+        return digitalDocumentDAO.updateDigitalDoc(digitalDocument);
     }
 }
